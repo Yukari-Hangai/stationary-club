f21d6cf33121a841ab88f7602e6e3339f5797f57
Yukari Hangai <129511468+Yukari-Hangai@users.noreply.github.com>
2023-06-11 08:22:12 +0000

    フォロー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -33,10 +33,11 @@ scope module: :public do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :update]
-      resource :relationships, only: [:create, :destroy]
+  resources :users, only: [:show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
 
 #管理者側ルーティング

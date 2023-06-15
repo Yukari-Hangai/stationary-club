d879a10128150229fa6ac9c15e75ed05925a8582
Yukari Hangai <129511468+Yukari-Hangai@users.noreply.github.com>
2023-06-15 04:16:28 +0000

    管理者側投稿削除機能追加

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -5,5 +5,11 @@ class Admin::PostsController < ApplicationController
     @user_posts = Post.where(user_id: @user.id).includes(:user).order("created_at DESC")
     @post_comment = PostComment.new
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_user_posts_path(@post.user_id)
+  end
   
 end

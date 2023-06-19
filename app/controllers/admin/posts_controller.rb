947ef426cfc8dcc5b62140c76cb90caada00ef1e
Yukari Hangai <129511468+Yukari-Hangai@users.noreply.github.com>
2023-06-19 09:29:44 +0000

    【レイアウト】管理者側投稿一覧　　実装ひとまず完了

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,7 +3,7 @@ class Admin::PostsController < ApplicationController
   
   def index
     @user = User.find(params[:user_id])
-    @user_posts = Post.where(user_id: @user.id).includes(:user).order("created_at DESC")
+    @user_posts = Post.where(user_id: @user.id).includes(:user).order("created_at DESC").page(params[:page])
     @post_comment = PostComment.new
   end
 
